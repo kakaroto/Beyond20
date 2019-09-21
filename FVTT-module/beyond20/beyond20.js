1f58911e9dde9488f0b7fffdc0bcbe4e71bf6a83
Youness Alaoui <kakaroto@kakaroto.homelinux.net>
2019-09-20 23:39:32 -0400

    Update FVTT Companion module
    Adds the underline CSS for dice total
    Show a warning notification if beyond20 wasn't activated after 5 seconds

--- FVTT-module/beyond20/beyond20.js
+++ FVTT-module/beyond20/beyond20.js
@@ -1,5 +1,11 @@
 Hooks.on('ready', function () {
-    if (typeof(chrome) !== "undefined" && typeof(browser) === "undefined") {
-        ui.notifications.info("If you are using Beyond20, remember to activate it for this game by clicking on the icon in the toolbar.")
+    if (typeof (chrome) !== "undefined" && typeof (browser) === "undefined") {
+        let message = "If you are using Beyond20, remember to activate it for this game by clicking on the icon in the toolbar."
+        ui.notifications.info(message)
+        setTimeout(() => {
+            if (document.title.startsWith("Foundry Virtual Tabletop")) {
+                ui.notifications.warn(message)
+            }
+        }, 5000)
     }
 })
