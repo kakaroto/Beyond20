4acebc7f12b9249dbdcbdf353bf7d6f8a60577cc
Jeremiah Altepeter <jeremiah@altepeter.com>
2020-04-16 18:19:10 -0700

    Revert "Change button text"
    This reverts commit ad38a65ae4a81b266f0786ef85e487ecb82592df.

--- src/roll20_script.js
+++ src/roll20_script.js
@@ -2558,8 +2558,7 @@ function all(iterable) {
 if (!all.__argnames__) Object.defineProperties(all, {
     __argnames__ : {value: ["iterable"]}
 });
-var decimal_sep, define_str_func, ρσ_unpack, ρσ_orig_split, ρσ_orig_replace;
-decimal_sep = 1.1.toLocaleString()[1];
+var define_str_func, ρσ_unpack, ρσ_orig_split, ρσ_orig_replace;
 function ρσ_repr_js_builtin(x, as_array) {
     var ans, b, keys, key;
     ans = [];
@@ -2936,7 +2935,7 @@ define_str_func("format", function () {
                     value = value.toExponential(prec - 1);
                 }
                 value = value.replace(/0+$/g, "");
-                if (value[value.length-1] === decimal_sep) {
+                if (value[value.length-1] === ".") {
                     value = value.slice(0, -1);
                 }
                 if (ftype === "G") {
@@ -4428,6 +4427,9 @@ var str = ρσ_str, repr = ρσ_repr;;
                             pos = close + 1;
                             continue;
                         }
+                        if (extension === "<") {
+                            throw new SyntaxError("Look behind assertions are not supported in JavaScript");
+                        }
                         if (extension === "(") {
                             throw new SyntaxError("Group existence assertions are not supported in JavaScript");
                         }
@@ -5180,9 +5182,7 @@ var str = ρσ_str, repr = ρσ_repr;;
             s.jsset.add("aside");
             s.jsset.add("audio");
             s.jsset.add("b");
-            s.jsset.add("base");
             s.jsset.add("big");
-            s.jsset.add("body");
             s.jsset.add("blockquote");
             s.jsset.add("br");
             s.jsset.add("button");
@@ -5221,7 +5221,6 @@ var str = ρσ_str, repr = ρσ_repr;;
             s.jsset.add("h5");
             s.jsset.add("h6");
             s.jsset.add("hr");
-            s.jsset.add("head");
             s.jsset.add("i");
             s.jsset.add("iframe");
             s.jsset.add("img");
