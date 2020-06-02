53a6774eae92c94d70c4b2bbf97b0e5a4f86b2d2
Youness Alaoui <kakaroto@kakaroto.homelinux.net>
2020-06-02 00:49:29 -0400

    Fix discord channel post-migration of setting

--- src/common/roll_renderer.js
+++ src/common/roll_renderer.js
@@ -317,7 +317,7 @@ class Beyond20RollRenderer {
 
         html += "</div>";
         const character = (request.whisper == WhisperType.HIDE_NAMES) ? "???" : request.character.name;
-        const discordChannel = (this._settings["discord-channels"] || []).find(c => c.active);
+        const discordChannel = getDiscordChannel(this._settings, request.character)
         postToDiscord(discordChannel ? discordChannel.secret : "", request, title, source, attributes, description, attack_rolls, roll_info, damage_rolls, total_damages, open).then(discord_error => {
             if (discord_error != undefined)
                 this._displayer.displayError("Beyond20 Discord Integration: " + discord_error);
@@ -744,7 +744,7 @@ class Beyond20RollRenderer {
 
     displayAvatar(request) {
         const character = (request.whisper == WhisperType.HIDE_NAMES) ? "???" : request.character.name;
-        const discordChannel = (this._settings["discord-channels"] || []).find(c => c.active);
+        const discordChannel = getDiscordChannel(this._settings, request.character)
         postToDiscord(discordChannel ? discordChannel.secret : "", request, request.name, "", {}, "", [], [], [], [], false).then((error) => {
             if (error)
                 this._displayer.displayError("Beyond20 Discord Integration: " + error);
--- src/common/settings.js
+++ src/common/settings.js
@@ -1021,6 +1021,14 @@ function getDiscordChannelsSetting(name) {
     console.log("Get Discord channels : ", channels);
     return channels;
 }
+
+function getDiscordChannel(settings, character) {
+    const channels = (settings["discord-channels"] || [])
+    if (typeof (channels) === "string")
+        return channels;
+    return channels.find(c => c.active);
+}
+
 options_list["vtt-tab"]["createHTMLElement"] = createVTTTabSetting;
 options_list["vtt-tab"]["set"] = setVTTTabSetting;
 options_list["vtt-tab"]["get"] = getVTTTabSetting;
