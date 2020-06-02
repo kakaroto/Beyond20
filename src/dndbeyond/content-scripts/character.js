02659d84e747d08eb6ca61306c006d9d9ef4f9c6
Aeristoka <jmgriffes@gmail.com>
2020-06-02 11:54:03 -0600

    Fix for Centaur Hooves

--- src/dndbeyond/content-scripts/character.js
+++ src/dndbeyond/content-scripts/character.js
@@ -454,7 +454,7 @@ function rollAction(paneClass) {
         // Polearm master bonus attack using the other end of the polearm is considered a melee attack.
         if (action_name == "Polearm Master - Bonus Attack" || action_name.includes("Unarmed Strike") || action_name == "Tavern Brawler Strike"
             || action_name.includes("Psychic Blade") || action_name.includes("Bite") || action_name.includes("Claws") || action_name.includes("Tail")
-            || action_name.includes("Ram") || action_name.includes("Horns")) {
+            || action_name.includes("Ram") || action_name.includes("Horns") || action_name.includes("Hooves")) {
             if (character.hasClassFeature("Fighting Style: Great Weapon Fighting"))
                 damages[0] = damages[0].replace(/[0-9]*d[0-9]+/g, "$&<=2");
             if (character.hasAction("Channel Divinity: Legendary Strike") &&
