ff86a265e0b976fada6e6d50dd8348370e735d76
Youness Alaoui <kakaroto@kakaroto.homelinux.net>
2021-03-06 02:24:09 -0500

    FVTT: look for foundry.js script to detect FVTT
    This is better as it works for games where a module changes the title
    Fixes #696

--- src/fvtt/check-tab.js
+++ src/fvtt/check-tab.js
@@ -1,5 +1,5 @@
 // Avoid injecting much code in pages that aren't FVTT so we just activate
 // the icon and let the background script inject the real FVTT script
-if (document.title.indexOf("Foundry Virtual Tabletop") !== -1) {
+if (document.querySelector("script[src='scripts/foundry.js']") !== null) {
     chrome.runtime.sendMessage({"action": "activate-icon"});
 }
