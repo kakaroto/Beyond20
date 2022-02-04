bc4eb601d8152fdd7d287f48f1e659b824f76366
jjchambl <jjchambliss@gmail.com>
2022-02-03 22:12:45 -0600

    fix missing mod in rendered roll

--- src/roll20/content-script.js
+++ src/roll20/content-script.js
@@ -741,6 +741,17 @@ async function handleOGLRenderedRoll(request) {
         atk_props["dmg2"] = convertRollToText(request.whisper, request.damage_rolls[1][1]);
         atk_props["dmg2type"] = request.damage_rolls[1][0];
     }
+    if (originalRequest.rollAttack && originalRequest["to-hit"] !== undefined) {
+        // let d20_roll = originalRequest.d20 || "1d20";
+        // if (originalRequest["critical-limit"])
+        //     d20_roll += "cs>" + originalRequest["critical-limit"];
+        let custom_roll_dice = "";
+        if (originalRequest.character && originalRequest.character.type === "Character")
+            custom_roll_dice = originalRequest.character.settings["custom-roll-dice"] || "";
+        atk_props["mod"] = originalRequest["to-hit"] + format_plus_mod(custom_roll_dice);
+        // properties["r1"] = genRoll(d20_roll, { "": originalRequest["to-hit"], "CUSTOM": custom_roll_dice });
+        // properties["attack"] = 1;
+    }
 
     message += template(request, atkType, atk_props) + "\n";
 
