kakaroto/Beyond20

commit 181d27d0074f4ab1a6508ba03040af8f5d293ce9

Fix `vtt` field in roll response to always be an array

src/dndbeyond/base/utils.js
@@ -645,7 +645,7 @@ function beyond20SendMessageFailure(character, response) {
     if (!response || (response.request && response.request.action === "update-combat"))
         return;
     console.log("Received response : ", response);
-    if (["roll", "rendered-roll"].includes(response.request.action)  && (response.vtt == "dndbeyond" || response.error)) {
+    if (["roll", "rendered-roll"].includes(response.request.action)  && (response.vtt[0] == "dndbeyond" || response.error)) {
         dndbeyondDiceRoller.handleRollError(response.request, response.error);
     } else if (response.error) {
         alertify.error("<strong>Beyond 20 : </strong>" + response.error);

src/extension/background.js
@@ -214,7 +214,7 @@ function onMessage(request, sender, sendResponse) {
         }
         const trackFailure = { "roll20": null, "fvtt": null, "custom": null }
         if (settings["vtt-tab"] && settings["vtt-tab"].vtt === "dndbeyond") {
-            sendResponse({ "success": false, "vtt": "dndbeyond", "error": null, "request": request })
+            sendResponse({ "success": false, "vtt": ["dndbeyond"], "error": null, "request": request })
         } else {
             sendMessageToRoll20(request, settings["vtt-tab"], makeFailureCB(trackFailure, "roll20", sendResponse))
             sendMessageToFVTT(request, settings["vtt-tab"], makeFailureCB(trackFailure, "fvtt", sendResponse))

commit 2f7da80a8e95e439ade8270a8a0cc40e7850b982

Fix broken fallback to ddb page
Was broken in 181d27d0074f4ab1a6508ba03040af8f5d293ce9

src/dndbeyond/base/utils.js
@@ -645,7 +645,7 @@ function beyond20SendMessageFailure(character, response) {
     if (!response || (response.request && response.request.action === "update-combat"))
         return;
     console.log("Received response : ", response);
-    if (["roll", "rendered-roll"].includes(response.request.action)  && (response.vtt[0] == "dndbeyond" || response.error)) {
+    if (["roll", "rendered-roll"].includes(response.request.action)  && ((response.vtt && response.vtt[0] == "dndbeyond") || response.error)) {
         dndbeyondDiceRoller.handleRollError(response.request, response.error);
     } else if (response.error) {
         alertify.error("<strong>Beyond 20 : </strong>" + response.error);