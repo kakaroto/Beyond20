kakaroto/Beyond20

commit 5ade9b42d2542b7059771c6b0db921036284cf81

Apply custom modifiers for wildshape creatures
first part of #1058

src/roll20/content-script.js
@@ -600,8 +600,10 @@ function convertRollToText(whisper, roll, standout=false) {
 
 async function handleRoll(request) {
     let custom_roll_dice = "";
-    if (request.character.type == "Character")
+    if (request.character.type == "Character"
+        (request.character.type == "Creature" && request.character.creatureType === "Wild Shape")) {
         custom_roll_dice = request.character.settings["custom-roll-dice"] || "";
+    }
     if (request.type == "skill") {
         roll = rollSkill(request, custom_roll_dice);
     } else if (request.type == "ability") {

src/common/roll_renderer.js
@@ -786,8 +786,10 @@ class Beyond20RollRenderer {
 
     handleRollRequest(request) {
         let custom_roll_dice = "";
-        if (request.character.type == "Character")
+        if (request.character.type == "Character" ||
+            (request.character.type == "Creature" && request.character.creatureType === "Wild Shape")) {
             custom_roll_dice = request.character.settings["custom-roll-dice"] || "";
+        }
 
         if (request.type == "avatar") {
             return this.displayAvatar(request);

commit fe2fa1e7004ccf04ed603c2ae1821c8cc36ce6c8

Fix roll20 support
Broken in 5ade9b42d2542b7059771c6b0db921036284cf81

src/roll20/content-script.js
@@ -600,7 +600,7 @@ function convertRollToText(whisper, roll, standout=false) {
 
 async function handleRoll(request) {
     let custom_roll_dice = "";
-    if (request.character.type == "Character"
+    if (request.character.type == "Character" ||
         (request.character.type == "Creature" && request.character.creatureType === "Wild Shape")) {
         custom_roll_dice = request.character.settings["custom-roll-dice"] || "";
     }