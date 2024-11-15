585aa1047ff09f8b04ef1ce9fb05775aeae9ac08
Youness Alaoui <kakaroto@kakaroto.ca>
2024-11-14 23:32:33 -0500

    Keep actor data compatible with foundry v9 and lower

--- FVTT-module/beyond20/beyond20.js
+++ FVTT-module/beyond20/beyond20.js
@@ -36,10 +36,17 @@ class Beyond20 {
                     user: game.user.id
                 }
             },
-            ownership: {
+        };
+        // In v10, permission field was changed into ownership
+        if (isNewerVersion(game.version || game.data.version, "10")) {
+            baseAttributes["ownership"] = {
                 [game.userId]: CONST.DOCUMENT_OWNERSHIP_LEVELS.OWNER
             }
-        };
+        } else {
+            baseAttributes["permission"] = {
+                [game.userId]: CONST.DOCUMENT_PERMISSION_LEVELS.OWNER
+            }
+        }
         if (!["Character", "Monster", "Creature"].includes(request.character.type)) {
             return {...baseAttributes, img: "icons/svg/mystery-man.svg", system: actorData, items: []}
         }
@@ -740,7 +747,7 @@ class Beyond20CreateNativeActorsApplication extends FormApplication {
         for (const user of game.users.contents) {
             const actor = game.actors.find(a => a.getFlag("beyond20", "user") === user.id);
             if (!actor) {
-                await Actor.create({
+                const actorData = {
                     name: user.name,
                     type: "character",
                     img: "modules/beyond20/images/icons/icon256.png",
@@ -749,11 +756,19 @@ class Beyond20CreateNativeActorsApplication extends FormApplication {
                         beyond20: {
                             user: user.id
                         }
-                    },
-                    ownership: {
+                    }
+                }
+                // In v10, permission field was changed into ownership
+                if (isNewerVersion(game.version || game.data.version, "10")) {
+                    actorData["ownership"] = {
                         [user.id]: CONST.DOCUMENT_OWNERSHIP_LEVELS.OWNER
                     }
-                });
+                } else {
+                    actorData["permission"] = {
+                        [user.id]: CONST.DOCUMENT_PERMISSION_LEVELS.OWNER
+                    }
+                }
+                await Actor.create(actorData);
                 ui.notifications.info(`Created Beyond20 Actor for user ${user.name}`);
             }
         }
