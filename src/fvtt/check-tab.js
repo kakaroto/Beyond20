05ee6580272f2c4881873e20daa88d85ea156631
David Martin <home@sirrio.de>
2025-06-05 02:39:46 +0200

    Support new Foundry script name

--- src/fvtt/check-tab.js
+++ src/fvtt/check-tab.js
@@ -1,5 +1,5 @@
 // Avoid injecting much code in pages that aren't FVTT so we just activate
 // the icon and let the background script inject the real FVTT script
-if (document.querySelector("script[src='scripts/foundry.js']") !== null) {
+if (document.querySelector("script[src='scripts/foundry.js'], script[src='scripts/foundry.mjs']") !== null) {
     chrome.runtime.sendMessage({"action": "activate-icon"});
 }
