9f6e53fc4698466b216f8f8f5f932c1ef8d3a9ab
kristiandueholm <kristian618@gmail.com>
2026-01-19 17:46:32 +0100

    Strip ending asterisks

--- src/dndbeyond/content-scripts/character.js
+++ src/dndbeyond/content-scripts/character.js
@@ -494,9 +494,10 @@ function handleSpecialMeleeAttacks(damages=[], damage_types=[], properties, sett
     // enhanced unarmed strike
     if(to_hit !== null && 
         character.hasFeat("Tavern Brawler 2024") &&
-        ["enhanced unarmed strike", "unarmed strike", "flurry of blows"].some(
-            unarmed_action => action_name.toLocaleLowerCase().includes(unarmed_action)
-        )) {
+        ["enhanced unarmed strike", "unarmed strike", "flurry of blows"].includes(
+            action_name.toLocaleLowerCase().trim().replace(/[\s*]+$/, "")
+        )
+    ) {
         damages[0] = damages[0].replace(/[0-9]*d[0-9]+/g, "$&ro<=1");
         effects.push("Tavern Brawler");
     }
